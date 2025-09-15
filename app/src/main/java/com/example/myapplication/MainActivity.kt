8ff8c9a9b145a53a8ecc9ee376d05ba6f41c97cb
VovaKalentev <ermakvova283@gmail.com>
2025-09-15 16:11:28 +0200

    ууу

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,17 +14,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
-    var num = 0
-    fun onClick(view: View) {
-        num += 1
-        var textView = findViewById<TextView>(R.id.text)
-        textView.text = "Кликов: $num"
     }
 
+//    var num = 0
+//    fun onClick(view: View) {
+//        num += 1
+//        var textView = findViewById<TextView>(R.id.text)
+//        textView.text = "Кликов: ${num}"
+//        if ( num==10)
+//        {
+//            var ll = findViewById<LinearLayout>(R.id.ll)
+//            val newText = TextView(this).apply {
+//                text = "Ёу!"
+//                layoutParams = LinearLayout.LayoutParams(
+//                    LinearLayout.LayoutParams.WRAP_CONTENT,
+//                    LinearLayout.LayoutParams.WRAP_CONTENT
+//                )
+//
+//            }
+//            ll.addView(newText);
+//            textView.text = "ЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁЁуЁЁЁЁу"
+//            num = 0
+//        }
+//    }
 }
